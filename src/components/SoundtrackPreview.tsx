575668a2a10bd005366e4fc086df4bd109d9bb36
Patrik <patrik.zlatic@gmail.com>
2025-09-08 21:37:11 +0200

    add stop button and functionality to SoundtrackPreview component

--- src/components/SoundtrackPreview.tsx
+++ src/components/SoundtrackPreview.tsx
@@ -1,6 +1,7 @@
 import useSound from "use-sound";
-import { FaPlay } from "react-icons/fa";
+import { FaPlay, FaStop } from "react-icons/fa";
 import { SOUNDTRACKS, type Soundtrack } from "../data/soundtracks";
+import { useEffect, useRef, useState } from "react";
 
 const FADE_DURATION_MS = 5000; // start fade-in 5s after start and start fade-out 5s before end
 const PREVIEW_END_MS = 20000; // completely stop after 20s
@@ -11,27 +12,59 @@ export default function SoundtrackPreview({
 }: {
   trackLabel: Soundtrack;
 }) {
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const timeoutsRef = useRef<number[]>([]);
   const [play, { stop, sound }] = useSound(SOUNDTRACKS[trackLabel], {
     html5: true,
     interrupt: true,
   });
+
+  useEffect(() => {
+    return () => {
+      timeoutsRef.current.forEach(clearTimeout);
+      timeoutsRef.current = [];
+      stop();
+    };
+  }, [stop]);
+
   return (
-    <button
-      type="button"
-      onClick={() => {
-        play();
-        sound?.once("play", (id: number) => {
-          sound.volume(0, id);
-          sound.fade(0, 1, FADE_DURATION_MS, id); // fade in
-          setTimeout(
-            () => sound.fade(1, 0, FADE_DURATION_MS, id),
-            FADE_OUT_START_MS
-          ); //fade out
-          setTimeout(() => stop(), PREVIEW_END_MS); // stop
-        });
-      }}
-    >
-      <FaPlay />
-    </button>
+    <>
+      {!isPlaying ? (
+        <button
+          type="button"
+          onClick={() => {
+            setIsPlaying(true);
+            play();
+            sound?.once("play", (id: number) => {
+              sound.volume(0, id);
+              sound.fade(0, 1, FADE_DURATION_MS, id); // fade in
+              const t1 = setTimeout(
+                () => sound.fade(1, 0, FADE_DURATION_MS, id),
+                FADE_OUT_START_MS
+              ); //fade out
+              const t2 = setTimeout(() => {
+                stop();
+                setIsPlaying(false);
+              }, PREVIEW_END_MS); // stop
+              timeoutsRef.current.push(t1, t2);
+            });
+          }}
+        >
+          <FaPlay />
+        </button>
+      ) : (
+        <button
+          type="button"
+          onClick={() => {
+            setIsPlaying(false);
+            timeoutsRef.current.forEach(clearTimeout);
+            timeoutsRef.current = [];
+            stop();
+          }}
+        >
+          <FaStop />
+        </button>
+      )}
+    </>
   );
 }
